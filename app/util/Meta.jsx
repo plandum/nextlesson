b45c1b69ccd7095fbacfb850331e170a60ac817b
plandum <temik.0465@mail.ru>
2022-11-06 20:48:35 +0300

    v1

---
+++ app/util/Meta.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Meta = ({title,  description}) => {
+  return <Head>
+      <title>{title}</title>
+      <meta 
+        name='description'
+        content={description}
+    />
+    <link rel="icon" href="/favicon.ico"/>
+    </Head>
+}
+
+export default Meta
